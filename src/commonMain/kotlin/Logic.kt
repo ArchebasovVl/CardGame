6229e676d4bc3d34869e49d1aecb64c26a87b92a
kilka-bez-hvosta <akobyannaira@gmail.com>
2024-05-19 22:39:31 +0300

    draft of game logic (2)

--- src/commonMain/kotlin/Logic.kt
+++ src/commonMain/kotlin/Logic.kt
@@ -2,9 +2,16 @@ import kotlin.random.Random
 
 // класс Desk который хранит карты которые на столе а так же проверяет полученные карты
 
+class Desk () {
+    private val cards: MutableList<Int> = mutableListOf()
+
+    fun getDeskstack
+
+}
+
 // класс Stack который хранит колоду возвращает последнюю карту берущему и забирает карты со стола
 class Stack (){
-    private val cards: MutableList<int> = mutableListOf()
+    private val cards: MutableList<Int> = mutableListOf()
     init {
         for (color in  1..4) {
             for (number in 0..9) {
@@ -14,20 +21,23 @@ class Stack (){
         cards.shuffle()
     }
 
-    fun getCard() {
+    fun getCard(desk: Desk): Int {
         if (cards.size!= 0) {
             val getting = cards[cards.size - 1]
             cards.removeLast()
             return getting
         }
         else {
-            //cards = getDeskstack().shuffle() // <- взять карты выброшенные на стол и перемешать
+            cards = desk.getDeskstack().shuffle() // <- взять карты выброшенные на стол и перемешать
+            val getting = cards[cards.size - 1]
+            cards.removeLast()
+            return getting
         }
     }
 }
 
 class Hand () {
-    private val cards: MutableList<int> = mutableListOf()
+    private val cards: MutableList<Int> = mutableListOf()
     fun initial(stack: Stack) { //я просто хз как это через init прописать
         for (count in 0..8) {
             cards.add(stack.getCard()) //Берет карту с колоды
@@ -37,6 +47,8 @@ class Hand () {
     fun takeCard(stack: Stack) {
         cards.add(stack.getCard())
     }
+
+    fun makeMove()
 
 
 }
