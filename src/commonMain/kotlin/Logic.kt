c1d051179a0b3c25eabb6cd17f262c99d9697f23
kilka-bez-hvosta <akobyannaira@gmail.com>
2024-05-19 20:05:05 +0300

    draft of game logic (not full)

---
+++ src/commonMain/kotlin/Logic.kt
@@ -0,0 +1,42 @@
+import kotlin.random.Random
+
+// класс Desk который хранит карты которые на столе а так же проверяет полученные карты
+
+// класс Stack который хранит колоду возвращает последнюю карту берущему и забирает карты со стола
+class Stack (){
+    private val cards: MutableList<int> = mutableListOf()
+    init {
+        for (color in  1..4) {
+            for (number in 0..9) {
+                cards.add(color*10 + number)
+            }
+        }
+        cards.shuffle()
+    }
+
+    fun getCard() {
+        if (cards.size!= 0) {
+            val getting = cards[cards.size - 1]
+            cards.removeLast()
+            return getting
+        }
+        else {
+            //cards = getDeskstack().shuffle() // <- взять карты выброшенные на стол и перемешать
+        }
+    }
+}
+
+class Hand () {
+    private val cards: MutableList<int> = mutableListOf()
+    fun initial(stack: Stack) { //я просто хз как это через init прописать
+        for (count in 0..8) {
+            cards.add(stack.getCard()) //Берет карту с колоды
+        }
+    }
+
+    fun takeCard(stack: Stack) {
+        cards.add(stack.getCard())
+    }
+
+
+}
